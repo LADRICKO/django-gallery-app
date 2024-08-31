31a01baddb998e418f3528d6a8af9af78095a714
Obada Akivie <greydot.42@protonmail.com>
2024-08-31 21:05:50 +0100

    fixed the url routing bug issue

--- gallery_project/urls.py
+++ gallery_project/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include
 from gallery.views import gallery_view
 from django.conf import settings
 from django.conf.urls.static import static
-import gallery
+from gallery import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', gallery_view, name='gallery'),
-    path('', include(gallery.urls)),
+    path('', include(urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
